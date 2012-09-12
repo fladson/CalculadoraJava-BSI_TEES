23e222539b3cddcb61cb1e1fba9e5c182d4cb8c2
Aragonense <aragonense@gmail.com>
2012-09-12 00:34:29 -0300

    EM ANDAMENTO - task 716: Implementar a Interface Calculadora http://bsi.cerescaico.ufrn.br/redmine/issues/show/716

---
+++ src/view/CalcInterface.java
@@ -0,0 +1,17 @@
+package view;
+
+public interface CalcInterface {
+	
+	public void somar();
+	
+	public void subtrair();
+	
+	public void multiplicar();
+	
+	public void dividir();
+	
+	public void zerarMostrador();
+	
+	
+
+}
