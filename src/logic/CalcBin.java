39d4c5d34bce2ee99be68550435af3a2da01352f
Aragonense <aragonense@gmail.com>
2012-09-15 09:43:06 -0300

    Inicio de implementação das classes das calculadoras especificas

---
+++ src/logic/CalcBin.java
@@ -0,0 +1,49 @@
+package logic;
+
+import view.CalcInterface;
+
+public class CalcBin implements CalcInterface {
+
+	@Override
+	public void somar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void subtrair() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void multiplicar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dividir() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void zerarMostrador() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void imprimirNaTela() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mostrarResultado() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
